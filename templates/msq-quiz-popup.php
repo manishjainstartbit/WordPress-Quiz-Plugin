ba88df71a08a38dfb5b98a7cb377032e27f8428b
Manisha Soni <manishasoni@startbitsolutions.com>
2025-08-28 11:53:10 +0530

    Created msq-quiz.php
    Added templates folder and msq-quiz-popup.php file for modal popup

---
+++ templates/msq-quiz-popup.php
@@ -0,0 +1,7 @@
+<div class="msq-popup">
+    <div class="msq-quiz-content">
+        <button class="msq-close-btn">close</button>
+        <div class="msq-question-section"></div>
+        <div class="msq-result-section" style="display:none;"></div>
+    </div>
+</div>
